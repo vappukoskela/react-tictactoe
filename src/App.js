f133c5296952a49b8a1e899ef5aaaa8a4c0ff0cc
vappukoskela <vappu.koskela@gmail.com>
2020-10-22 16:48:49 +0300

    Using arrays

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  // array-pohjainen ratkaisu
+
+  const [pelaaja, setPelaaja] = useState("X"); 
+  const [headerMsg, setHeaderMsg] = useState("Vuorossa pelaaja " + pelaaja);
+  const [boardArray, setBoardArray] = useState([null, null, null,
+                                                null, null, null, 
+                                                null, null, null]);
+
+  const squareClick=(squareNo) => {
+    if(boardArray[squareNo]===null){
+      var tempArray = boardArray;
+      tempArray[squareNo] = pelaaja;
+      setBoardArray(tempArray);
+      if(isWinningMove()){
+        setHeaderMsg(pelaaja + " VOITTAA!")
+      } else if (!tempArray.includes(null)){
+        setHeaderMsg("TASAPELI");
+      } else {
+        switchPlayers();
+      }
+    }
+  }
+
+  const isWinningMove = () => {
+    //winningarrays on array of arrays kaikista mahdollisista voittajakombinaatioista
+    var winningArrays = [[boardArray[0],boardArray[1],boardArray[2]], [boardArray[3],boardArray[4],boardArray[5]], [boardArray[6],boardArray[7],boardArray[8]], 
+                         [boardArray[0],boardArray[3],boardArray[6]], [boardArray[1],boardArray[4],boardArray[7]], [boardArray[2],boardArray[5],boardArray[8]],
+                         [boardArray[0],boardArray[4],boardArray[8]], [boardArray[2],boardArray[4],boardArray[6]]]
+    for (var i = 0; i < winningArrays.length; i++){
+      if(winningArrays[i][0] === winningArrays[i][1] && winningArrays[i][1] === winningArrays[i][2] && winningArrays[i][0] != null){
+        return true; // jos kaikki voittajakombinaatioarrayn jäsenet ovat samat, voittaja löytyi
+      }
+    }
+    return false;
+  }
+
+  const switchPlayers = () => {
+    if(pelaaja === "X"){
+      setPelaaja("O");
+      setHeaderMsg("Vuorossa pelaaja O");
+    } else { 
+      setPelaaja("X"); 
+      setHeaderMsg("Vuorossa pelaaja X");
+    }
+  }
+  
+  const reset=() => {
+    setPelaaja("X"); 
+    setBoardArray([null, null, null,
+                   null, null, null, 
+                   null, null, null]);
+  }
+
+  // JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="ticTacToeHeader">{headerMsg}</div>
+      <div class="ticTacToe">
+        <div class="column">
+          <span class="square" onClick={() => squareClick(0)}>{boardArray[0]}</span>
+          <span class="square" onClick={() => squareClick(1)}>{boardArray[1]}</span>
+          <span class="square" onClick={() => squareClick(2)}>{boardArray[2]}</span>
+        </div>
+        <div class="column">
+          <span class="square" onClick={() => squareClick(3)}>{boardArray[3]}</span>
+          <span class="square" onClick={() => squareClick(4)}>{boardArray[4]}</span>
+          <span class="square" onClick={() => squareClick(5)}>{boardArray[5]}</span>
+        </div>
+        <div class="column">
+          <span class="square" onClick={() => squareClick(6)}>{boardArray[6]}</span>
+          <span class="square" onClick={() => squareClick(7)}>{boardArray[7]}</span>
+          <span class="square" onClick={() => squareClick(8)}>{boardArray[8]}</span>
+        </div>
+      </div>
+      <button class="resetButton" onClick={() => reset()}>Uusi peli</button>
     </div>
   );
 }
-
 export default App;
