82ed9a688250ac96373b7c62b877a0f024168350
vappukoskela <vappu.koskela@gmail.com>
2020-10-23 13:21:56 +0300

    using concat to make sure copying array to temp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  // array-pohjainen ratkaisu
-
-  const [pelaaja, setPelaaja] = useState("X"); 
+  const [pelaaja, setPelaaja] = useState("X");
   const [headerMsg, setHeaderMsg] = useState("Vuorossa pelaaja " + pelaaja);
   const [boardArray, setBoardArray] = useState([null, null, null,
-                                                null, null, null, 
+                                                null, null, null,
                                                 null, null, null]);
 
-  const squareClick=(squareNo) => {
-    if(boardArray[squareNo]===null){
-      var tempArray = boardArray;
+  const squareClick = (squareNo) => {
+    if (boardArray[squareNo] === null) {
+      var tempArray = [].concat(boardArray);
       tempArray[squareNo] = pelaaja;
       setBoardArray(tempArray);
-      if(isWinningMove()){
+      if (isWinningMove()) {
         setHeaderMsg(pelaaja + " VOITTAA!")
-      } else if (!tempArray.includes(null)){
+      } else if (!tempArray.includes(null)) {
         setHeaderMsg("TASAPELI");
       } else {
         switchPlayers();
@@ -29,11 +27,11 @@ function App() {
 
   const isWinningMove = () => {
     //winningarrays on array of arrays kaikista mahdollisista voittajakombinaatioista
-    var winningArrays = [[boardArray[0],boardArray[1],boardArray[2]], [boardArray[3],boardArray[4],boardArray[5]], [boardArray[6],boardArray[7],boardArray[8]], 
-                         [boardArray[0],boardArray[3],boardArray[6]], [boardArray[1],boardArray[4],boardArray[7]], [boardArray[2],boardArray[5],boardArray[8]],
-                         [boardArray[0],boardArray[4],boardArray[8]], [boardArray[2],boardArray[4],boardArray[6]]]
-    for (var i = 0; i < winningArrays.length; i++){
-      if(winningArrays[i][0] === winningArrays[i][1] && winningArrays[i][1] === winningArrays[i][2] && winningArrays[i][0] != null){
+    var winningArrays = [[boardArray[0], boardArray[1], boardArray[2]], [boardArray[3], boardArray[4], boardArray[5]], [boardArray[6], boardArray[7], boardArray[8]],
+                         [boardArray[0], boardArray[3], boardArray[6]], [boardArray[1], boardArray[4], boardArray[7]], [boardArray[2], boardArray[5], boardArray[8]],
+                         [boardArray[0], boardArray[4], boardArray[8]], [boardArray[2], boardArray[4], boardArray[6]]]
+    for (var i = 0; i < winningArrays.length; i++) {
+      if (winningArrays[i][0] === winningArrays[i][1] && winningArrays[i][1] === winningArrays[i][2] && winningArrays[i][0] != null) {
         return true; // jos kaikki voittajakombinaatioarrayn jäsenet ovat samat, voittaja löytyi
       }
     }
@@ -41,26 +39,23 @@ function App() {
   }
 
   const switchPlayers = () => {
-    if(pelaaja === "X"){
+    if (pelaaja === "X") {
       setPelaaja("O");
       setHeaderMsg("Vuorossa pelaaja O");
-    } else { 
-      setPelaaja("X"); 
+    } else {         
+      setPelaaja("X");
       setHeaderMsg("Vuorossa pelaaja X");
     }
   }
-  
-  const reset=() => {
+
+  const reset = () => {
     setHeaderMsg("Vuorossa pelaaja X");
-    setPelaaja("X"); 
+    setPelaaja("X");
     setBoardArray([null, null, null,
-                   null, null, null, 
+                   null, null, null,
                    null, null, null]);
   }
 
-
-
-  // JSX
   return (
     <div>
       <div class="ticTacToeHeader">{headerMsg}</div>
